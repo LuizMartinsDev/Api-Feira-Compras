33c629c7de2022da4eecc597ed64fe53509a6f0d
LuizMartinsDev <luizkwdev@gmail.com>
2024-10-04 12:52:36 -0300

    criação do método post

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
-import express from 'express';
+import express, {json} from 'express';
 
 const api = express();
+
+api.use(json())
+
 const port = 5000;
 
-api.get('/',(req, res) => {
-    res.send('Hello ')
+const items = []
+
+api.post('/items', (req, res) => {
+    const {name, quantity, type} = req.body;
+    const verificaNomeRepetido = items.find((item) => item.name === name);
+
+    if(verificaNomeRepetido){
+        return res.status(409).send('Não é possível salvar dois produtos com o mesmo nome')
+    }
+
+    if(!name || !quantity || !type){
+        return res.status(422).send("Todos os campos precisam ser enviados")
+    }
+    items.push({id: items.length + 1, name, quantity, type})
+    res.status(201).json({name, quantity, type})
 } )
 
 
